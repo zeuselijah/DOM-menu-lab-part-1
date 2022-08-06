27457fb98add38b2123f80b1ed421e451d127445
zeuselijah <simeonhendricks@gmail.com>
2022-08-06 10:35:12 -0400

    task 2 complete

--- js/script.js
+++ js/script.js
@@ -20,5 +20,32 @@ mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
 // Task 1.3
 // Add a class of flex-ctrto mainEl.
 
-mainEl.classList.add('flex-ctrto');
+mainEl.classList.add('flex-ctr');
 console.log(mainEl);
+
+// Task 2.0
+// Select and cache the <nav id="top-menu"> element 
+// in a variable named topMenuEl.
+
+const topMenuEl = document.getElementById('top-menu');
+//console.log(topMenuEl);
+
+// Task 2.1
+// Set the height topMenuElelement to be 100%.
+
+topMenuEl.style.height = "100%";
+//console.log(topMenuEl);
+
+//Task 2.2
+//Set the background color of topMenuEl 
+//to the value stored in the --top-menu-bg CSS custom property.
+
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+
+//Task 2.3
+//Add a class of flex-around to topMenuEl.
+
+topMenuEl.classList.add('flex-around');
+console.log(topMenuEl);
+
